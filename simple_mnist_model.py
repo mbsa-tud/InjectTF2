78117d47cd2958ad3dd331cc985a317fbcb3f8ec
M-Beyer <23377889+M-Beyer@users.noreply.github.com>
2019-11-07 11:30:46 +0100

    Fixed model.

--- simple_mnist_model.py
+++ simple_mnist_model.py
@@ -16,47 +16,29 @@ save_path = "./model"
 model_name = "simple_mnist_model.h5"
 # - - - - - - - - - - - - - - -
 
-# Load MNIST dataset
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-# Add a channels dimension
-x_train = x_train[..., tf.newaxis]
-x_test = x_test[..., tf.newaxis]
 
-# Define the Model
-class MyModel(Model):
-    def __init__(self):
-        super(MyModel, self).__init__()
-        self.conv1 = Conv2D(32, 3, activation="relu")
-        self.flatten = Flatten()
-        self.d1 = Dense(128, activation="relu")
-        self.d2 = Dense(10, activation="softmax")
-
-    def call(self, x):
-        x = self.conv1(x)
-        x = self.flatten(x)
-        x = self.d1(x)
-        return self.d2(x)
-
-
-# Create an instance of the model
-model = MyModel()
+model = tf.keras.models.Sequential(
+    [
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10, activation="softmax"),
+    ]
+)
 
 model.compile(
     optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
 )
 
-# Train the Model
 model.fit(x_train, y_train, epochs=5)
 
-# Evaluate the Model
 model.evaluate(x_test, y_test, verbose=2)
 
-# Print Model summary
-model.summary()
-
-# Save the Model
 model.save(save_path + "/" + model_name)
+
+model.summary()
