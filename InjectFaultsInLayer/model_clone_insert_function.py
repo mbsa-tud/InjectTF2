822507896dbdcc61ebb9bf2e66fb4063f5826d53
Emil Valiev <mr.mimlka@mail.ru>
2019-11-11 14:00:30 +0100

    Operation injection for further study

--- InjectFaultsInLayer/model_clone_insert_function.py
+++ InjectFaultsInLayer/model_clone_insert_function.py
@@ -73,7 +73,7 @@ def cloneFunction(layer):
     #     wrapped_layer = LayerWrapper.LayerWrapperDense(layer)
     # if type(layer) is Dropout:
     #     wrapped_layer = LayerWrapper.LayerWrapperDropout(layer)
-    #wrapped_layer = LayerWrapper.LayerWrapper(layer)
+    # wrapped_layer = LayerWrapper.LayerWrapper(layer)
 
     return Lambda(lambda x: (layer.call(x)) * 0)
 
@@ -84,29 +84,51 @@ clonedModel = tf.keras.models.clone_model(
     clone_function=cloneFunction
 )
 
+# model.fit(x_train, y_train, epochs=5)
 
-
-#model.fit(x_train, y_train, epochs=5)
-
-#model.evaluate(x_test, y_test, verbose=2)
+# model.evaluate(x_test, y_test, verbose=2)
 
 # clonedModel.compile(
 #      optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
 # )
 
 clonedModel.summary()
-pred_previous = np.argmax(model.predict(x_test[0,tf.newaxis]))
-pred = np.argmax(clonedModel.predict(x_test[0,tf.newaxis]))
+pred_previous = np.argmax(model.predict(x_test[0, tf.newaxis]))
+pred = np.argmax(clonedModel.predict(x_test[0, tf.newaxis]))
 truth = y_test[0]
 
 print('Y value without faults: {0} predicted value: {1}'.format(y_test[0], pred_previous))
 
 print('Y value with faults: {0} predicted value: {1}'.format(y_test[0], pred))
 
+# region why this doesnt work
+
+g = tf.Graph()
 
 
-#clonedModel.fit(x_train, y_train, epochs=5)
+@tf.function
+def custom_operation(x):
+    return x*0
 
-#clonedModel.evaluate(x_test, y_test, verbose=2)
+
+with g.as_default():
+    m = tf.keras.models.load_model("./InjectFaultsInLayer/model/" + "testModel.h5")
+    operations = m._graph.get_operations()
+    i_max = len(operations)
+    i = 0
+    while i < i_max:
+        if operations[i].type == 'Mul':
+            operations[i] = custom_operation(0)
+        i += 1
+    post_pred = np.argmax(m.predict(x_test[0, tf.newaxis]))
+    m.summary()
+
+    print('Y value with faults in ops: {0} predicted value: {1}'.format(y_test[0], post_pred))
+
+# endregion
+
+# clonedModel.fit(x_train, y_train, epochs=5)
+
+# clonedModel.evaluate(x_test, y_test, verbose=2)
 
 # new_model = tf.keras.models.load_model(model_name)
