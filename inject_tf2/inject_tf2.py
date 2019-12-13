fef3f5306de19e674c0fc126e3cfcef337b09045
M-Beyer <23377889+M-Beyer@users.noreply.github.com>
2019-12-13 09:54:08 +0100

    Updated golden run function, ModelManager object and defined `input_data` as a TensorFlow DataSet.

--- inject_tf2/inject_tf2.py
+++ inject_tf2/inject_tf2.py
@@ -17,73 +17,67 @@ from inject_tf2.config_manager import ConfigurationManager
 
 class InjectTF2:
     def __init__(
-        self, path_to_model, path_to_config, input_data, logging_level="ERROR"
+        self,
+        path_to_model,
+        path_to_config,
+        tf_dataset,
+        batch_size,
+        logging_level="ERROR",
     ):
 
-        # Setup logging
+        # Setup logging.
         logging.basicConfig()
         logging.getLogger().setLevel(logging_level)
         logging.debug("Logging level set to {0}".format(logging_level))
 
         self.im = InjectionManager()
-        self.mm = ModelManager(path_to_model)
         self.cm = ConfigurationManager(path_to_config)
 
-        self.input_data = input_data
+        # Create a batched dataset. Since the values for the layer that is selected
+        # for injection are stored later on, `drop_remainder` is set to `True`
+        # (that way a uniform numpy can be pre-allocated).
+        self.batched_ds = tf_dataset.batched(batch_size, drop_remainder=True)
 
-        # TODO
-        # Validate user inputs
+        self.mm = ModelManager(
+            path_to_model, self.cm.get_selected_layer(), self.batched_ds, batch_size
+        )
 
         self.golden_run = self._execute_golden_run(input_data)
-        self.golden_run_layers = self._execute_golden_run_layers(input_data)
 
-    def _execute_golden_run(self, data):
-        return self.mm.get_org_model().predict(data)
+    def _execute_golden_run(self, batched_tf_dataset):
+        return self.mm.get_org_model().evaluate(batched_tf_dataset)
 
-    def _execute_golden_run_layers(self, data):
-        result = []
-
-        for i, layer_model in enumerate(self.mm.get_layer_models()):
-
-            # First iteration. Pass the dataset into the first layer.
-            if not result:
-                result.append(layer_model.predict(data))
-
-            # Use the output values from the previous layer
-            else:
-                result.append(layer_model.predict(result[i - 1]))
-
-        return result
-
+    # TODO Refactor, DEPRECATED
     def run_experiments(self, input_data):
 
-        results = []
+        # results = []
 
-        for i, layer in enumerate(self.mm.get_layer_models()):
+        # for i, layer in enumerate(self.mm.get_layer_models()):
 
-            if self.cm.is_selected_for_inj(i + 1, layer):
+        #     if self.cm.is_selected_for_inj(i + 1, layer):
 
-                # Compute output of layer
-                # If first layer
-                if i == 0:
-                    output_val = layer.predict(input_data)
+        #         # Compute output of layer
+        #         # If first layer
+        #         if i == 0:
+        #             output_val = layer.predict(input_data)
 
-                else:
-                    output_val = layer.predict(results[i - 1])
+        #         else:
+        #             output_val = layer.predict(results[i - 1])
 
-                inj_res = self.im.inject(
-                    output_val, self.cm.get_config_for_layer(i + 1, layer)
-                )
+        #         inj_res = self.im.inject(
+        #             output_val, self.cm.get_config_for_layer(i + 1, layer)
+        #         )
 
-                results.append(inj_res)
+        #         results.append(inj_res)
 
-            else:
+        #     else:
 
-                # If first layer
-                if i == 0:
-                    results.append(layer.predict(input_data))
+        #         # If first layer
+        #         if i == 0:
+        #             results.append(layer.predict(input_data))
 
-                else:
-                    results.append(layer.predict(results[i - 1]))
+        #         else:
+        #             results.append(layer.predict(results[i - 1]))
 
-        return results
+        # return results
+        return
