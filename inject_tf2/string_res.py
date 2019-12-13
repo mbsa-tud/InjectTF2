d7082020b91c6772cba9bde75fbc315e898aebdf
M-Beyer <23377889+M-Beyer@users.noreply.github.com>
2019-12-13 09:51:09 +0100

    Updated string resources.

--- inject_tf2/string_res.py
+++ inject_tf2/string_res.py
@@ -1,20 +1,17 @@
 # - resource file for strings
 
 # region key words for config
-
-probability_str = 'probability'
-fault_type_str = 'fault_type'
-inject_layer_number_str = 'inject_layer_number'
-elements_str = 'elements'
-
+inject_layer_str = "inject_layer"
+layer_name_str = "layer_name"
+probability_str = "probability"
+fault_type_str = "fault_type"
+# elements_str = "elements"         # TODO Refactor, deprecated
 # endregion
 
-# region key words for faults
-
-bit_flip_str = 'BitFlip'
-random_str = 'Random'
-zero_str = 'Zero'
-all_values_str = 'all'
-random_values_str = 'random'
-
+# region key words for fault_type key
+bit_flip_str = "BitFlip"
+# random_str = "Random"             # TODO Refactor, deprecated
+# zero_str = "Zero"                 # TODO Refactor, deprecated
+# all_values_str = "All"            # TODO Refactor, deprecated
+# random_values_str = "Random"      # TODO Refactor, deprecated
 # endregion
