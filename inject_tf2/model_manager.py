9c638f4a29a250a5f4fc7ba4c9d7e6a89e9d9331
M-Beyer <23377889+M-Beyer@users.noreply.github.com>
2019-12-13 10:28:58 +0100

    Added prediction function for the selected layer, minor refactoring.

--- inject_tf2/model_manager.py
+++ inject_tf2/model_manager.py
@@ -13,22 +13,22 @@ from tensorflow.python.keras import backend as K
 
 class ModelManager:
     def __init__(self, path_to_model, layer_name, batched_tf_dataset, batch_size):
-        self.org_model = tf.keras.models.load_model(path_to_model)
+        self._org_model = tf.keras.models.load_model(path_to_model)
 
-        self.layer_output_values = self._get_layer_output_values_for_layer(
-            self.org_model, layer_name, batched_tf_dataset, batch_size
+        self._selected_layer = self._org_model.get_layer(layer_name)
+
+        self._layer_output_values = self._get_layer_output_values_for_layer(
+            batched_tf_dataset, batch_size
         )
 
-    def _get_layer_output_values_for_layer(
-        self, model, layer_name, batched_tf_dataset, batch_size
-    ):
+    def _get_layer_output_values_for_layer(self, batched_tf_dataset, batch_size):
 
         # Get the output tensor for the selected layer.
-        layer_output_tensor = model.get_layer(layer_name).output
+        layer_output_tensor = self._selected_layer.output
 
         # Create a function which evaluates the output tensor for
         # the selected layer and returns its values
-        functor = K.function(model.inputs, layer_output_tensor)
+        functor = K.function(self._org_model.inputs, layer_output_tensor)
 
         # Allocate memory for the output values
         num_of_batches = 0
@@ -53,3 +53,8 @@ class ModelManager:
         """Returns the output values for the selected layer
         for the provided test data."""
         return self.layer_output_values
+
+    def predict_func_from_layer(self):
+        """Returns a prediction function which feed its input argument into the
+        selected layer and returns the prediction of the model."""
+        return K.function(self._selected_layer.output, self._org_model.outputs)
