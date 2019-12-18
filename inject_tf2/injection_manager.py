1d549e5c57954889ca45d203c77cbf4c66720e4c
M-Beyer <23377889+M-Beyer@users.noreply.github.com>
2019-12-18 12:10:43 +0100

    Restructured InjectionManager.
    Removed the other (unsused, not tested) fault injections methods `zero`
    and `random`.

--- inject_tf2/injection_manager.py
+++ inject_tf2/injection_manager.py
@@ -5,33 +5,42 @@
 # TU-Dresden, Institute of Automation (IfA)
 #
 
-import inject_tf2.inject_tf2_util as itfutil
-import inject_tf2.string_res as str_res
 import logging
 import numpy as np
+
+import inject_tf2.inject_tf2_util as itfutil
+import inject_tf2.string_res as str_res
 
 
 class InjectionManager:
     def __init__(self):
         logging.debug("initialized InjectionManager")
 
-    def inject(self, output_val, layer_inj_config):
+    def inject_batch(self, batch_output_values, layer_inj_config):
 
-        # TODO
-        # - Generate random number, compare with provided injection probability
-        random_number = np.random.rand()
-        if layer_inj_config[str_res.probability_str] > random_number:
-            # - Call correct injection function based on configuration
-            return self._injected_functions[layer_inj_config[str_res.fault_type_str]](
-                output_val,
-                layer_inj_config
-            )
+        # Get the probability for fault injection for the selected layer.
+        layer_inj_probability = layer_inj_config[str_res.inject_layer_str][
+            str_res.probability_str
+        ]
 
-        # - Return default output of layer
-        return output_val
+        # For each sample in the batch generate a random number and compare it
+        # to the specified fault injection probabilty.
+        for i, sample in enumerate(batch_output_values):
+            random_number = np.random.rand()
+
+            if layer_inj_probability > random_number:
+
+                # Call correct injection function based on the configuration.
+                result = self._injected_functions[
+                    layer_inj_config[str_res.inject_layer_str][str_res.fault_type_str]
+                ](sample, layer_inj_config)
+
+                batch_output_values[i] = result
+
+        return batch_output_values
 
     @staticmethod
-    def _get_BitFlip_from_value(element_val):
+    def _get_bit_flipped_value(element_val, bit_number=None):
         """Flips one random bit of a value and returns it."""
 
         # convert float according to IEEE 754,
@@ -39,49 +48,39 @@ class InjectionManager:
         # convert back to float
         if element_val.dtype == np.float32:
 
-            # select random bit
-            bit_num = np.random.randint(0, 32)
+            if bit_number is None:
+                # No specific bit specified, select random bit
+                bit_number = np.random.randint(0, 32)
+
             element_val_bin = int(itfutil.float_to_bin32(element_val), 2)
-            element_val_bin ^= 1 << bit_num
+            element_val_bin ^= 1 << bit_number
             element_val_bin = bin(element_val_bin)[2:].zfill(32)
             element_val = itfutil.bin_to_float32(element_val_bin)
 
         elif element_val.dtype == np.float64:
 
-            # select random bit
-            bit_num = np.random.randint(0, 64)
+            if bit_number is None:
+                # No specific bit specified, select random bit
+                bit_number = np.random.randint(0, 64)
+
             element_val_bin = int(itfutil.float_to_bin64(element_val), 2)
-            element_val_bin ^= 1 << bit_num
+            element_val_bin ^= 1 << bit_number
             element_val_bin = bin(element_val_bin)[2:].zfill(64)
             element_val = itfutil.bin_to_float64(element_val_bin)
 
         else:
-            raise NotImplementedError("Bit flip is not supported for dtype: ", element_val.dtype)
+            raise NotImplementedError(
+                "Bit flip is not supported for dtype: ", element_val.dtype
+            )
 
         return element_val
 
     @staticmethod
-    def _injectBitFlip_all(tensor):
-        """Flips one random bit of an each element of the tensor and returns the updated tensor."""
-        dimensions = []
-        for dimension in tensor.shape:
-            dimensions.append(dimension)
+    def _inject_bit_flip_random_element(tensor, bit_number=None):
+        """Flips one bit of a random element of the tensor and returns the updated tensor.
 
-        if len(dimensions) == 1:
-            # Do BitFlip for each value takes so much time, so just bitflipping one random value in each dimension
-            # for i in range(len(tensor)):
-                #tensor[i] = InjectionManager._get_BitFlip_from_value(tensor[i])
-            index = np.random.randint(0,len(tensor))
-            tensor[index] = InjectionManager._get_BitFlip_from_value(tensor[index])
-        else:
-            for i in range(len(tensor)):
-                tensor[i] = InjectionManager._injectBitFlip_all(tensor[i])
-
-        return tensor
-
-    @staticmethod
-    def _injectBitFlip_random(tensor):
-        """Flips one random bit of a random element of the tensor and returns the updated tensor."""
+        If `bit_number = None`, a random bit will be flipped. For `bit_number = i`,
+        the ith bit will be flipped (zero indexed)."""
 
         # select random element from tensor by generating
         # random indices based on tensor dimensions
@@ -89,71 +88,40 @@ class InjectionManager:
         for dimension in tensor.shape:
             element.append(np.random.randint(0, dimension))
 
-        element_val = InjectionManager._get_element(tensor, *element)
+        element_val = InjectionManager._get_element_from_tensor(tensor, *element)
 
-        new_element_val = InjectionManager._get_BitFlip_from_value(element_val)
+        new_element_val = InjectionManager._get_bit_flipped_value(
+            element_val, bit_number
+        )
 
-        tensor = InjectionManager._set_element(new_element_val, tensor, *element)
+        tensor = InjectionManager._set_element_in_tensor(
+            new_element_val, tensor, *element
+        )
         return tensor
 
     @staticmethod
-    def _injectBitFlip(tensor, inj_config):
-        """Flips one random bit of a random element of the tensor and returns the updated tensor."""
+    def _inject_bit_flip(tensor, inj_config):
+        """Handle the bit flip injection based on the selected bit flip type."""
 
-        if inj_config[str_res.elements_str] == str_res.random_values_str:
-            return InjectionManager._injectBitFlip_random(tensor)
+        # Determine the bit flip type that should be executed
+        bf_type = inj_config[str_res.inject_layer_str][str_res.bit_flip_type_str]
+
+        if bf_type == str_res.specific_bit_str:
+
+            # Get the bit number
+            bit_num = inj_config[str_res.inject_layer_str][str_res.bit_number_str]
+            return InjectionManager._inject_bit_flip_random_element(tensor, bit_num)
+
         else:
-            return InjectionManager._injectBitFlip_all(tensor)
+            return InjectionManager._inject_bit_flip_random_element(tensor)
 
     @staticmethod
-    def _random_tensor(tensor, inj_config):
-        """Replacement of a tensor value with random value"""
-        # The tensor.shape is a tuple, while rand needs linear arguments
-        # So we need to unpack the tensor.shape tuples as arguments using *
-
-        # If needs to replace only one random value
-        if inj_config[str_res.elements_str] == str_res.random_values_str:
-            # Generate random value for injection
-            random_value = np.random.rand(1)
-            # Inject random_value in random position into random index
-            dimensions = []
-            for dimension in tensor.shape:
-                dimensions.append(np.random.randint(0, dimension))
-            injected_result = InjectionManager._set_element(random_value, tensor, *dimensions)
-            return injected_result
-        # If needs to replace all values with random values
-        else:
-            return np.random.rand(*tensor.shape)
+    def _get_element_from_tensor(tensor, *e_indices):
+        return tensor[e_indices]
 
     @staticmethod
-    def _zero_tensor(tensor, inj_config):
-        """Replacement of a tensor value with zero"""
-        # The tensor.shape is a tuple, while rand needs linear arguments
-        # So we need to unpack the tensor.shape tuples as arguments using *
+    def _set_element_in_tensor(value, tensor, *e_indices):
+        tensor[e_indices] = value
+        return tensor
 
-        # If needs to replace with zero only one random value
-        if inj_config[str_res.elements_str] == str_res.random_values_str:
-            # Inject zero value in random position into random index
-            dimensions = []
-            for dimension in tensor.shape:
-                dimensions.append(np.random.randint(0, dimension))
-            injected_result = InjectionManager._set_element(0.0, tensor, *dimensions)
-            return injected_result
-        # If needs to replace all values with zeros
-        else:
-            return np.zeros(tensor.shape)
-
-    @staticmethod
-    def _get_element(tens, *e_indices):
-        return tens[e_indices]
-
-    @staticmethod
-    def _set_element(val, tens, *e_indices):
-        tens[e_indices] = val
-        return tens
-
-    _injected_functions = {
-        str_res.bit_flip_str: _injectBitFlip.__func__,
-        str_res.random_str: _random_tensor.__func__,
-        str_res.zero_str: _zero_tensor.__func__,
-    }
+    _injected_functions = {str_res.bit_flip_str: _inject_bit_flip.__func__}
--- inject_tf2/string_res.py
+++ inject_tf2/string_res.py
@@ -1,17 +1,22 @@
 # - resource file for strings
 
-# region key words for config
+# region
+# key words for config file
 inject_layer_str = "inject_layer"
 layer_name_str = "layer_name"
 probability_str = "probability"
 fault_type_str = "fault_type"
-# elements_str = "elements"         # TODO Refactor, deprecated
+bit_flip_type_str = "bit_flip_type"
+bit_number_str = "bit_number"
 # endregion
 
-# region key words for fault_type key
+# region
+# values for `fault_type` key
 bit_flip_str = "BitFlip"
-# random_str = "Random"             # TODO Refactor, deprecated
-# zero_str = "Zero"                 # TODO Refactor, deprecated
-# all_values_str = "All"            # TODO Refactor, deprecated
-# random_values_str = "Random"      # TODO Refactor, deprecated
+# endregion
+
+# region
+# values for `bit_flip_type` key
+random_bit_str = "RandomBit"
+specific_bit_str = "SpecificBit"
 # endregion
