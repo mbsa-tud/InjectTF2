6197fa0060bc75ebe6f7d53aede808bc0323d470
M-Beyer <23377889+M-Beyer@users.noreply.github.com>
2020-07-15 00:00:30 +0200

    Removed unused files.

--- run_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-docker run --rm \
-          -it \
-          -p 8888:8888 \
-          -e JUPYTER_ENABLE_LAB=yes \
-          --name injectTF_dev \
-          -v "$(pwd):/home/jovyan/InjectTF_dev" \
-          nvaitc/ai-lab:19.10-tf2 \
-          bash
